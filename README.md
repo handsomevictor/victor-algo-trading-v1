68517d0ddc7361fab387106e1dec4499f9e76a50
Handsome Victor <857730556@qq.com>
2023-03-17 00:32:08 +0100

    updates

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # victor-algo-trading-v1
  For v1, build a good monitor dashboard and develop a strategy building class.
+
+目标：
+在GCP上存储数据（动态数据，目前打算存储coinmarketcap上前10名的coin在最大的几家交易所上的trade和ohlcv等数据）
+同时直接GCP上跑模型和algo策略，然后上传influxdb（host在GCP上）上，然后用grafana展示，展示的还有策略的收益买卖点等
+
+数据源：kaiko，我自己的api
---
+++ sdk_data/indices_sdk.py
@@ -0,0 +1,37 @@
+from __future__ import print_function
+import logging
+import os
+
+import grpc
+from google.protobuf.json_format import MessageToJson
+
+from kaikosdk import sdk_pb2_grpc
+from kaikosdk.stream.index_v1 import request_pb2 as pb_index
+
+
+def index_request(channel: grpc.Channel):
+    try:
+        with channel:
+            stub = sdk_pb2_grpc.StreamIndexServiceV1Stub(channel)
+            responses = stub.Subscribe(pb_index.StreamIndexServiceRequestV1(
+                index_code="KK_RR_BTCUSD"
+            ))
+            for response in responses:
+                print("Received message %s" % (MessageToJson(response, including_default_value_fields=True)))
+
+    except grpc.RpcError as e:
+        print(e.details(), e.code())
+
+
+def run():
+    credentials = grpc.ssl_channel_credentials(root_certificates=None)
+    call_credentials = grpc.access_token_call_credentials(os.environ['KAIKO_API_KEY'])
+    composite_credentials = grpc.composite_channel_credentials(credentials, call_credentials)
+    channel = grpc.secure_channel('gateway-v0-grpc.kaiko.ovh', composite_credentials)
+
+    index_request(channel)
+
+
+if __name__ == '__main__':
+    logging.basicConfig()
+    run()
